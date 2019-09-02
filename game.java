c8f88bfc6aa42f4032bbcd83beb292dd3fd25bb8
[opkl206] <[opkl206@naver.com]>
2019-09-02 15:49:31 +0900

    1909021550

--- game.java
+++ game.java
@@ -1,28 +1,24 @@
 package ex0502;
 
-import java.util.Random;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.Font;
-import javax.swing.*;
+import java.awt.Graphics;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ItemListener;
-import java.awt.Frame;
-import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.Toolkit;
 import java.awt.event.ItemEvent;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.event.ItemListener;
+import java.util.Random;
 
-import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 public class game extends JFrame implements ActionListener, ItemListener, Runnable {
    
@@ -70,7 +66,7 @@ class MyPuzzle1 extends JFrame implements ActionListener {
    JButton btn[];
    JButton reset;
    JPanel panel,back;
-   JLabel back1;
+   JLabel back1,ex;
    Font myfont;
    JScrollPane scrollPane;
    ImageIcon icon2;
@@ -94,9 +90,14 @@ class MyPuzzle1 extends JFrame implements ActionListener {
          btn[i].setForeground(Color.blue);
          btn[i].addActionListener(this);
          panel.add(btn[i]);
-
+        // if(btn[0].getIcon().equals(icon[0]) || btn[1].getIcon().equals(icon[1])) {
+        //	// JOptionPane.showMessageDialog(this,"큰 판 위에 작은 판을 올리세요 ","Tower Of Hanoi",JOptionPane.ERROR_MESSAGE);
+        //	 System.out.println("하이");
+         //}
 
       }
+      
+    	  
       btn[8].setIcon(null);
       reset = new JButton("Reset");
       reset.setFont(myfont);
@@ -268,6 +269,7 @@ class MyPuzzle1 extends JFrame implements ActionListener {
          }
          if(e.getSource()==reset)
          {
+        	 
             int u[]=MyPuzzle1.makeRnum();
             for(int i=0; i<8; i++)
             {
@@ -277,6 +279,11 @@ class MyPuzzle1 extends JFrame implements ActionListener {
             }
             btn[8].setIcon(null);
             }
+//         while(false) {
+//        	 if(btn[0].getIcon().equals(icon[0]) || btn[1].getIcon().equals(icon[1])) {
+//        		 JOptionPane.showMessageDialog(this,"큰 판 위에 작은 판을 올리세요 ","Tower Of Hanoi",JOptionPane.ERROR_MESSAGE);
+//        	 }
+//         }
          
          }
    
